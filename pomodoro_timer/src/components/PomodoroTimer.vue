21c1fe0f8c3ce1515206cf9ea6c8563b91f2002c
thosonit <thoson.it@gmail.com>
2025-10-21 23:37:02 +0700

    update UI

---
+++ pomodoro_timer/src/components/PomodoroTimer.vue
@@ -0,0 +1,16 @@
+<script></script>
+
+<template>
+  <div class="container"></div>
+</template>
+
+<style scoped>
+.container {
+  width: 300px;
+  height: 320px;
+  background: #1e1e1e;
+  border: #353535 8px solid;
+  border-radius: 16px;
+  box-shadow: 0 0 20px rgba(0, 0, 0, 0.5);
+}
+</style>
